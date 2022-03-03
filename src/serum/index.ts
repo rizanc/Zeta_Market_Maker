d5182892723ea413ce183c952ff1d70829d6cd7b
Costin Rizan <rizanc@gmail.com>
2022-03-03 14:49:16 -0500

    Testing the SERUM orderbook

---
+++ src/serum/index.ts
@@ -0,0 +1,199 @@
+require("dotenv").config();
+
+import { Account, Connection, PublicKey } from '@solana/web3.js';
+import { Market } from '@project-serum/serum';
+import { mainModule } from 'process';
+import { OrderParams } from '@project-serum/serum/lib/market';
+import BN from 'bn.js';
+
+enum TransactionType {
+    BUY_SOL = 0, SELL_SOL = 1, NO_ORDER = 2
+}
+
+const sol_account = process.env.pub_key;
+const usdc_account = process.env.usdc_account;
+const connection = new Connection('https://solana-api.projectserum.com');
+
+// SOL/USDC
+const marketAddress = new PublicKey('9wFFyRfZBsuAha4YcuxcXLKwMxJR43S7fPfQLusDBzvT');
+
+// Serum Dex Program v3 
+const programAddress = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
+
+async function main() {
+
+    let market = await Market.load(connection, marketAddress, {}, programAddress);
+
+    // setInterval(async () => {
+    // Fetching orderbooks
+    let bids = await market.loadBids(connection);
+    let asks = await market.loadAsks(connection);
+
+    // L2 orderbook data
+    let sortedAsks = asks.getL2(7).sort((a, b) => b[0] - a[0]);
+    for (let [price, size] of sortedAsks) {
+        console.log(price, size);
+    }
+
+    console.log("===============================");
+
+    let sortedBids = bids.getL2(7);
+    for (let [price, size] of sortedBids) {
+        console.log(price, size);
+    }
+    console.log('\n\n\n\n');
+
+    // }, 3000);
+
+    const privateKey: string = process.env.PRIVATE_KEY!; // stored as an array string
+    const keypair = new Account(Uint8Array.from(JSON.parse(privateKey)));
+
+    // Placing orders
+    let owner = keypair;
+
+    let transactionType: TransactionType;
+
+
+    let order: OrderParams<Account>;
+    let payer;
+
+    /*
+        (0) 90.4 329.8
+        90.286 2000
+        90.264 1575.9
+        90.158 3503.8
+        90.122 1000
+        90 2.5
+        (6) 89.96 300 <== INSIDE ASK
+        ===============================
+        (0) 89.773 1  <== INSIDE BID
+        89.734 300
+        89.659 1000
+        89.6 334.5
+        89.517 2328.1
+        89.502 1
+        (6) 89.5 24.9
+    */
+
+
+    transactionType = TransactionType.NO_ORDER;
+    let clientId = Date.now().toString();
+    switch (+transactionType) {
+        case TransactionType.SELL_SOL:
+            payer = new PublicKey(sol_account);
+            order = {
+                owner,
+                payer,
+                side: "sell",
+                price: +sortedBids[0][0] + 0.01,
+                size: 5,
+                orderType: "postOnly", // 'limit', 'ioc', 'postOnly',
+                clientId: new BN(clientId)
+            };
+
+            break;
+        case TransactionType.BUY_SOL:
+            payer = new PublicKey(usdc_account);
+            order = {
+                owner,
+                payer,
+                side: "buy", // 'buy' or 'sell'
+                price: +sortedBids[0][0] + 0.02,
+                size: 4,
+                orderType: "postOnly", // 'limit', 'ioc', 'postOnly'
+                clientId: new BN(clientId)
+            };
+            break;
+        default:
+            break;
+
+    }
+
+    if (order) {
+        console.log(order);
+
+        let res = await market.placeOrder(connection, order);
+        console.log(res);
+    }
+
+    //DRQNx76FW3e5yf8fNJ9BYfG9rnQrqEBvfsFWDJCtq9KkUcYTN6QEyYeDnNCmKJyDrtHjHSBZ3Yi1mJ2RmFNSeDe
+    // Full orderbook data
+    // for (let order of asks) {
+    //     console.log(
+    //         order.orderId,
+    //         order.price,
+    //         order.size,
+    //         order.side, // 'buy' or 'sell'
+    //     );
+    // }
+
+
+
+    setInterval(async () => {
+
+
+        for (let openOrders of await market.findOpenOrdersAccountsForOwner(
+            connection,
+            owner.publicKey,
+        )) {
+            console.log(openOrders.address.toString());
+            console.log(`Base  Free ${openOrders.baseTokenFree.toNumber()} \tTotal: ${openOrders.baseTokenTotal.toNumber()}`);
+            console.log(`Quote Free ${openOrders.quoteTokenFree.toNumber()} \tTotal: ${openOrders.quoteTokenFree.toNumber()}`);
+            console.log("\n\n\n\n");
+
+            if (openOrders.baseTokenFree.toNumber() > 0 || openOrders.quoteTokenFree.toNumber() > 0) {
+                // spl-token accounts to which to send the proceeds from trades
+                let baseTokenAccount = new PublicKey(sol_account); // owner.publicKey;
+                let quoteTokenAccount = new PublicKey(usdc_account);
+
+                let res = await market.settleFunds(
+                    connection,
+                    owner,
+                    openOrders,
+                    baseTokenAccount,
+                    quoteTokenAccount,
+                );
+
+                console.log(`Settled funds transaction: ${res}`);
+            }
+        }
+
+        let myOrders = await market.loadOrdersForOwner(connection, owner.publicKey);
+        console.log("myOrders");
+        for (let order of myOrders) {
+
+            console.log(order.orderId.toString(), order.clientId.toString(), order.price, order.size, order.side);
+
+        }
+
+        console.log("Fills")
+        for (let fill of await market.loadFills(connection)) {
+            console.log(fill.orderId, fill.price, fill.size, fill.side);
+        }
+
+        // console.log("Cancels")
+        for (let order of myOrders) {
+            let res = await market.cancelOrder(connection, owner, order);
+            console.log(res);
+        }
+
+    }, 15000);
+
+
+
+
+    // console.log("Fills")
+    // for (let fill of await market.loadFills(connection)) {
+    //     console.log(fill.orderId, fill.price, fill.size, fill.side);
+    // }
+
+    //     console.log("Cancels")
+    //     for (let order of myOrders) {
+    //         let res = await market.cancelOrder(connection, owner, order);
+    //         console.log(res);
+    //     }
+}
+
+
+main();
+
